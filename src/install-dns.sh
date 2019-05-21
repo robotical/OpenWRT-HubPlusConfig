fd6b48c6853f54ceab6defd921eeb09f609d248f
Angus Pearson <angus@toaster.cc>
2019-05-21 13:24:00 +0100

    Use configureable LAN IP address

--- src/install-dns.sh
+++ src/install-dns.sh
@@ -2,12 +2,13 @@
 ## Install script for Robotical Command Hub+
 
 . ./VERSION
+. ./CONFIG
 
 echo "+   Adding DNS entries to DHCP config"
 
 uci set dhcp.dashboard=domain
 uci set dhcp.dashboard.name='hub.robotical.io'
-uci set dhcp.dashboard.ip='192.168.8.1'
+uci set dhcp.dashboard.ip=$HUB_LAN_IPADDR
 
 uci set dhcp.scratchlink=domain
 uci set dhcp.scratchlink.name='device-manager.scratch.mit.edu'
@@ -15,11 +16,11 @@ uci set dhcp.scratchlink.ip='127.0.0.1'
 
 uci set dhcp.scratchcdn=domain
 uci set dhcp.scratchcdn.name='cdn.assets.scratch.mit.edu'
-uci set dhcp.scratchcdn.ip='192.168.8.1'
+uci set dhcp.scratchcdn.ip=$HUB_LAN_IPADDR
 
 uci set dhcp.scratchmain=domain
 uci set dhcp.scratchmain.name='scratch.mit.edu'
-uci set dhcp.scratchmain.ip='192.168.8.1'
+uci set dhcp.scratchmain.ip=$HUB_LAN_IPADDR
 
 uci commit dhcp
 
